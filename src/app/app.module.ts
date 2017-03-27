94f7828ac911a70c507656c2cb916849f2024568
chikakow <chikakow@gmail.com>
2017-03-26 21:15:43 -0400

    component files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { JokeComponent } from './joke/joke.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    JokeComponent
   ],
   imports: [
     BrowserModule,
